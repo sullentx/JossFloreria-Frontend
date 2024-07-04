c568df9099df9ac7977940e42fe01090c33db6cf
Maax9o9 <maxrockogenial@gmail.com>
2024-07-03 22:12:06 -0600

    home view

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/layout/header';
-
 import Footer from './components/layout/footer';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
       {
-
+        <Home />
       }
       <Footer />
     </div>

95700639b226ab8202754486632ba1f66199ebcf
Maax9o9 <maxrockogenial@gmail.com>
2024-07-03 20:40:37 -0600

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-// src/App.jsx
 import React from 'react';
 import Header from './components/layout/header';
-import './App.css';
+import Footer from './components/layout/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       {
-
-
+        
       }
-
+      <Footer />
     </div>
   );
 }

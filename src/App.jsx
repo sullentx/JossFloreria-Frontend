5fc464e65546a076a281272a36580de6b07b3ce0
Yara-Molina <yara24315@gmail.com>
2024-07-03 22:14:21 -0600

    About Us view

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@
 import React from 'react';
 import Header from './components/layout/header';
 import './App.css';
-
+import AboutUsSection from './components/organims/AboutUsSection';
 function App() {
   return (
     <div className="App">
       <Header />
-      {
-
-
-      }
-
+   <main>
+    <AboutUsSection/>
+   </main>
     </div>
   );
 }
